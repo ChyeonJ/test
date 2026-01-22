309f72727c47912485f2cb25a1567d2bad3477a1
ChyeonJ <akdntm4757@gmail.com>
2026-01-22 19:48:37 +0900

    음료수 자판기

--- java_exam/src/oop/DrinkMachineArray.java
+++ java_exam/src/oop/DrinkMachineArray.java
@@ -27,25 +27,48 @@ public class DrinkMachineArray {
 	 *모든 상품 재고 출력() : void
 	 *출력형식  “박카스 15개 남았습니다.”
 	 */
-	
+	//주문하기(제품 번호, 주문수량): 구매금액(int)
 	public int orderDrinK(int count, int stock) {
+		
+		
+		// 이상한 숫자를 넣어도 오류 출력 해야함
+		if(count >= this.drinkM.length) {
+			System.out.println("없는 상품");
+			return 0;
+		}
+		//재고가 없다면 "상품이 품절되었습니다" 출력하고 0을 반환한다.	=> 이것을 먼저 검증하는게 좋을듯 빠른연산
 		
 		int totalStock = this.drinkM[count].stock - stock;
 		
-		// 이상한 숫자를 넣어도 오류 출력 해야함
-		
-		//재고가 없다면 "상품이 품절되었습니다" 출력하고 0을 반환한다.	=> 이것을 먼저 검증하는게 좋을듯 빠른연산
-		if (stock == 0 || totalStock == 0) {
-			
+		if(stock == 0 || totalStock > this.drinkM[count].stock) {
+			System.out.println("상품이 품절되었습니다.");
+			totalStock = 0;
+			return 0;
+		}else{
+			int totalPrice = this.drinkM[count].price * stock; //상품의 가격 계산
+			this.drinkM[count].stock = totalStock;	// 주문을하면 주문 수량만큼 자판기 음료수의 재고가 감소
+			return totalPrice; //몬스터를 5개 주문한다면 7500을 반환한다.
 		}
-		//몬스터를 5개 주문한다면 7500을 반환한다.
-		
-		// 주문을하면 주문 수량만큼 자판기 음료수의 재고가 감소
-		
-
-		
-		return 0;
 	}
 	
+	// *입고하기(제품번호, 입고수량): void
+	public void addProduct(int count, int stock) {
+		
+		// 이상한 숫자를 넣어도 오류 출력 해야함
+		if(count > this.drinkM.length) {
+			System.out.println("없는 상품");
+			return;
+		//입고를 하면 입고 수량만큼 재고가 증가된다.
+		}else {
+			this.drinkM[count].stock += stock;	// 밀키스 3개를 입고하면, 밀키스의 재고가 3개 증가된다.
+		}
+	}
+	
+	//모든 상품 재고 출력() : void
+	public void printAllDrink() {
+		for(int i = 0; i < this.drinkM.length; i++) {
+			System.out.println(this.drinkM[i].drinkName + " " + this.drinkM[i].stock + "개 남았습니다.");
+		}
+	}
 
 }
